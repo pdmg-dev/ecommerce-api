b4304a3c7a0300d5efd04facedc9e761f6ed671d
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 12:58:06 +0800

    feat: create get_current_user dependency

---
+++ app/core/dependencies.py
@@ -0,0 +1,34 @@
+# app/core/dependencies.py
+
+from fastapi import Depends, HTTPException, status
+from jose import JWTError
+from sqlalchemy.orm import Session
+
+from app.core.security import oauth2_scheme
+from app.db.deps import get_db
+from app.models.user import User
+from app.repositories.user import UserRepository
+from app.utils.auth import decode_access_token
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+) -> User:
+    try:
+        payload = decode_access_token(token)
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+
+        user = UserRepository(db).get_by_email(email)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return user
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
