42fe511f5dac04a01c6c2ee850488cf08d163eaa
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 15:26:22 +0800

    feat: add relationship of CartItem to user

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 # app/models/user.py
-
+from typing import List
 from sqlalchemy import String, Integer, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db import Base
 
 class User(Base):
@@ -12,3 +12,5 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+    cart_items: Mapped[List["CartItem"]] = relationship(back_populates="user")
