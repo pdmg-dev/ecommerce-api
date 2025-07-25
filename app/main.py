0902673266731c979f9bf884d2ce198dfd159d9b
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 12:20:51 +0800

    feat: add db initialization to entry point

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+# app/main.py
+
 from fastapi import FastAPI
 from app.core.settings import get_settings
+from app.db.init_db import init_db
+
 
 settings = get_settings()
 
 app = FastAPI(title=settings.app_name, debug=settings.debug)
+init_db()
+
 
 @app.get("/")
 def read_root():
