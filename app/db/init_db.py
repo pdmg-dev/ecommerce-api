36aab2cd6a69e0ab6633e4476d99a16ed476e883
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 14:30:23 +0800

    feat: add product model for db initialization

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,7 +1,7 @@
 # app/db/init_db.py
 
 from app.db import Base, engine
-from app.models import user  # ensure model is imported
+from app.models import user, product  # ensure model is imported
 
 def init_db():
     Base.metadata.create_all(bind=engine)
