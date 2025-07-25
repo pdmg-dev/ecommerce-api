e8ea91f87b2505ed228c849764cf2d8037fdbdbd
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 12:23:58 +0800

    feat: add User models mapped in "users" table

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+# app/models/user.py
+
+from sqlalchemy import String, Integer, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
