8d4cfc98e7e2e90f9b55a641122a7835abcb457f
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 12:23:24 +0800

    feat: implements database config and initialization

---
+++ app/db/__init__.py
@@ -0,0 +1,19 @@
+# app/db/__init__.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from app.core.settings import get_settings
+
+settings = get_settings()
+
+engine = create_engine(
+    settings.database_url,
+    connect_args={"check_same_thread": False} if "sqlite" in settings.database_url else {},
+    echo=settings.debug
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
