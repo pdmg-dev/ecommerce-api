e479fbb6d4f20005e8928a129e4137cc0b2b3d44
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 23:44:50 +0800

    feat: add query for getting all orders, get order by id, and update order status

--- app/repositories/order.py
+++ app/repositories/order.py
@@ -3,7 +3,7 @@
 from sqlalchemy.orm import Session
 
 from app.models.cart_item import CartItem
-from app.models.order import Order, OrderItem
+from app.models.order import Order, OrderItem, OrderStatus
 from app.models.product import Product
 
 
@@ -17,8 +17,12 @@ class OrderRepository:
     def get_product_by_id(self, product_id: int):
         return self.db.query(Product).filter_by(id=product_id).first()
     
+    
     def get_orders_by_user(self, user_id: int):
         return self.db.query(Order).filter_by(user_id=user_id).order_by(Order.created_at.desc()).all()
+    
+    def get_all_orders(self):
+        return self.db.query(Order).order_by(Order.created_at.desc()).all()
 
     def create_order(self, user_id: int, total_price: float, items: list[OrderItem]):
         order = Order(user_id=user_id, total_price=total_price, items=items)
@@ -33,3 +37,11 @@ class OrderRepository:
 
     def refresh(self, instance):
         self.db.refresh(instance)
+        
+    def get_order_by_id(self, order_id: int) -> Order:
+        return self.db.query(Order).filter_by(id=order_id).first()
+        
+    def update_order_status(self, order: Order) -> Order:
+        self.db.commit()
+        self.db.refresh(order)
+        return order
