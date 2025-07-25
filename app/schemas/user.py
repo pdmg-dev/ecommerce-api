c0ee267dcb0e1d3666b7aab4711c0abc53c7b04e
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 12:29:10 +0800

    feat: add User schemas for data and type validation of user data

---
+++ app/schemas/user.py
@@ -0,0 +1,22 @@
+# app/schemas/user.py
+
+from pydantic import BaseModel, EmailStr
+
+# For incoming registration/login requests
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+# For outgoing (response to client)
+class UserRead(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    is_admin: bool
+
+    class Config:
+        from_attributes = True  # Needed for SQLAlchemy model conversion
