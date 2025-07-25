ed83a30f53ba830673e1f4136319574e0ecc00bc
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-25 12:46:19 +0800

    feat: include auth route to enrty point

--- app/main.py
+++ app/main.py
@@ -3,12 +3,16 @@
 from fastapi import FastAPI
 from app.core.settings import get_settings
 from app.db.init_db import init_db
+from app.api import auth
 
 
 settings = get_settings()
-
 app = FastAPI(title=settings.app_name, debug=settings.debug)
+
 init_db()
+
+# include routes
+app.include_router(auth.router)
 
 
 @app.get("/")
