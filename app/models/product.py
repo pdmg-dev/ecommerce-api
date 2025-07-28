45b444339cd78b98ecca899c25a9a4c342a4ef83
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-28 14:59:20 +0800

    feat: make the product name field unique

--- app/models/product.py
+++ app/models/product.py
@@ -11,7 +11,7 @@ class Product(Base):
     __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=True)
     price: Mapped[float] = mapped_column(Float, nullable=False)
     quantity: Mapped[int] = mapped_column(Integer, nullable=False)
