c7b62dacc552b1c360beccd1b3bb3d10da0cbe80
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-28 13:21:10 +0800

    refactor: use depedencies for repository and service

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -8,30 +8,43 @@ from app.core.security import oauth2_scheme
 from app.db.deps import get_db
 from app.models.user import User
 from app.repositories.user import UserRepository
+from app.services.user import UserService
+from app.utils import exceptions
 from app.utils.auth import decode_access_token
-from app.utils.exception import Exception
+
+
+# User Dependencies
+def get_user_repository(db: Session = Depends(get_db)) -> UserRepository:
+    return UserRepository(db)
+
+
+def get_user_service(
+    repo: UserRepository = Depends(get_user_repository),
+) -> UserService:
+    return UserService(repo)
 
 
 def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+    token: str = Depends(oauth2_scheme),
+    user_repository: UserRepository = Depends(get_user_repository),
 ) -> User:
     try:
         payload = decode_access_token(token)
         email: str = payload.get("sub")
         if email is None:
-            raise Exception.unauthorized("Invalid token")
+            raise exceptions.unauthorized("Invalid token")
 
-        user = UserRepository(db).get_by_email(email)
+        user = user_repository.get_by_email(email)
         if not user:
-            raise Exception.not_found("User not found")
+            raise exceptions.not_found("User not found")
 
         return user
 
     except JWTError:
-        raise Exception.unauthorized("Invalid or expired token")
+        raise exceptions.unauthorized("Invalid or expired token")
 
 
 def require_admin(current_user: User = Depends(get_current_user)) -> User:
     if not current_user.is_admin:
-        raise Exception.forbidden("Admin privileges required")
+        raise exceptions.forbidden("Admin privileges required")
     return current_user
