97c39a2eb01c86291a415a8901e17518380f1dc8
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-28 19:39:13 +0800

    refactor: use cart service dependency instead

--- app/api/cart.py
+++ app/api/cart.py
@@ -2,52 +2,52 @@
 
 from typing import List
 
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, status
 
-from app.core.dependencies import get_current_user, get_db
+from app.core.dependencies import get_cart_service, get_current_user
 from app.models.user import User
 from app.schemas.cart_item import CartItemCreate, CartItemRead, CartItemUpdate
-from app.services.cart_item import CartItemService
+from app.services.cart import CartService
 
 router = APIRouter(prefix="/cart", tags=["Cart"])
 
 
-@router.get("/", response_model=List[CartItemRead])
-def get_cart_items(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
-):
-    service = CartItemService(db)
-    return service.list_user_cart(current_user.id)
-
-
-@router.post("/", response_model=CartItemRead, status_code=201)
-def add_cart_item(
-    item: CartItemCreate,
-    db: Session = Depends(get_db),
+@router.get("/", response_model=List[CartItemRead], status_code=status.HTTP_200_OK)
+def list_cart_items(
+    cart_service: CartService = Depends(get_cart_service),
     current_user: User = Depends(get_current_user),
 ):
-    service = CartItemService(db)
-    return service.add_to_cart(current_user.id, item)
+    return cart_service.list_cart_items(current_user.id)
 
 
-@router.put("/{product_id}", response_model=CartItemRead)
+@router.post("/", response_model=CartItemRead, status_code=status.HTTP_201_CREATED)
+def add_cart_item(
+    create_data: CartItemCreate,
+    cart_service: CartService = Depends(get_cart_service),
+    current_user: User = Depends(get_current_user),
+):
+    return cart_service.add_item(current_user.id, create_data)
+
+
+@router.put(
+    "/{product_id}", response_model=CartItemRead, status_code=status.HTTP_200_OK
+)
 def update_cart_item(
     product_id: int,
-    item: CartItemUpdate,
-    db: Session = Depends(get_db),
+    update_data: CartItemUpdate,
+    cart_service: CartService = Depends(get_cart_service),
     current_user: User = Depends(get_current_user),
 ):
-    service = CartItemService(db)
-    return service.update_cart_item(current_user.id, product_id, item)
+    return cart_service.update_item_quantity(
+        current_user.id, product_id, update_data
+    )
 
 
-@router.delete("/{product_id}", status_code=204)
+@router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
 def remove_cart_item(
     product_id: int,
-    db: Session = Depends(get_db),
+    cart_service: CartService = Depends(get_cart_service),
     current_user: User = Depends(get_current_user),
 ):
-    service = CartItemService(db)
-    service.remove_from_cart(current_user.id, product_id)
-    return
+    cart_service.remove_item(current_user.id, product_id)
+    return 
