2aa54bd175f1a98e2c4585cd1989b5529e24b45a
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-29 13:54:53 +0800

    refactor: organize checkout route

--- app/api/order.py
+++ app/api/order.py
@@ -1,51 +1,39 @@
 # app/api/order.py
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from app.core.dependencies import get_current_user, get_db, require_admin
+from app.core.dependencies import get_current_user, require_admin
 from app.models.user import User
 from app.schemas.order import OrderRead, OrderUpdateStatus
 from app.services.order import OrderService
+from app.core.dependencies import get_order_service
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
-
-@router.post("/checkout", response_model=OrderRead)
-def checkout(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
+@router.get("/", response_model=List[OrderRead], status_code=status.HTTP_200_OK)
+def list_orders(
+    order_service: OrderService = Depends(get_order_service),
+    current_user: User = Depends(get_current_user)
 ):
-    service = OrderService(db)
-    try:
-        return service.checkout(current_user.id)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-
-@router.get("/", response_model=List[OrderRead])
-def list_user_orders(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
-):
-    service = OrderService(db)
-    return service.get_user_orders(current_user.id)
-
+    return order_service.list_orders(current_user.id)
 
 @router.get("/all", response_model=List[OrderRead])
 def get_all_orders(
-    db: Session = Depends(get_db),
+    order_service: OrderService = Depends(get_order_service),
     current_user: User = Depends(require_admin),
 ):
-    service = OrderService(db)
-    return service.list_all_orders()
+    return order_service.list_all_orders()
 
 
-@router.patch("/{order_id}", response_model=OrderRead)
-def update_order_status(
-    order_id: int,
-    status_data: OrderUpdateStatus,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(require_admin),
-):
-    service = OrderService(db)
-    return service.change_order_status(order_id, status_data.status)
+#@router.patch("/{order_id}", response_model=OrderRead)
+#def update_order_status(
+#    order_id: int,
+    #status_data: OrderUpdateStatus,
+    #db: Session = Depends(get_db),
+    #current_user: User = Depends(require_admin),
+#):
+    
+    #service = OrderService(db)
+    #return service.change_order_status(order_id, status_data.status)
