fc264c2f6bac787ff4430662df6d4c543d69011c
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-07-30 11:13:44 +0800

    feat: create route for admin only to update the status of orders

--- app/api/order.py
+++ app/api/order.py
@@ -12,12 +12,14 @@ from app.core.dependencies import get_order_service
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
+
 @router.get("/", response_model=List[OrderRead], status_code=status.HTTP_200_OK)
 def list_orders(
     order_service: OrderService = Depends(get_order_service),
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
 ):
     return order_service.list_orders(current_user.id)
+
 
 @router.get("/all", response_model=List[OrderRead])
 def get_all_orders(
@@ -27,13 +29,11 @@ def get_all_orders(
     return order_service.list_all_orders()
 
 
-#@router.patch("/{order_id}", response_model=OrderRead)
-#def update_order_status(
-#    order_id: int,
-    #status_data: OrderUpdateStatus,
-    #db: Session = Depends(get_db),
-    #current_user: User = Depends(require_admin),
-#):
-    
-    #service = OrderService(db)
-    #return service.change_order_status(order_id, status_data.status)
+@router.patch("/{order_id}", response_model=OrderRead)
+def update_order_status(
+    order_id: int,
+    status_data: OrderUpdateStatus,
+    order_service: OrderService = Depends(get_order_service),
+    current_user: User = Depends(require_admin),
+):
+    return order_service.update_order_status(order_id, status_data.status)
